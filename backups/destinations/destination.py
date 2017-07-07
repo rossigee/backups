3821e395268796f6f2a065bdd330598f5e8a8038
Ross Golder <ross@golder.org>
2017-07-07 10:36:04 +0700

    Fix for broken parsing of retention days/copies configuration settings.

--- backups/destinations/destination.py
+++ backups/destinations/destination.py
@@ -7,18 +7,22 @@ class BackupDestination:
 
         self.hostname = config.get_or_envvar('defaults', 'hostname', 'BACKUPS_HOSTNAME')
 
+        self.retention_copies = 0
         try:
             self.retention_copies = int(config.get('s3', 'retention_copies'))
         except:
-            try:
-                self.retention_copies = int(config.get('defaults', 'retention_copies', 'RETENTION_COPIES'))
-            except:
-                self.retention_copies = 0
+            pass
+        try:
+            self.retention_copies = int(config.get('defaults', 'retention_copies', 'RETENTION_COPIES'))
+        except:
+            pass
 
+        self.retention_days = 0
         try:
             self.retention_days = int(config.get('s3', 'retention_days'))
         except:
-            try:
-                self.retention_days = int(config.get('defaults', 'retention_days', 'RETENTION_DAYS'))
-            except:
-                self.retention_days = 0
+            pass
+        try:
+            self.retention_days = int(config.get('defaults', 'retention_days', 'RETENTION_DAYS'))
+        except:
+            pass
