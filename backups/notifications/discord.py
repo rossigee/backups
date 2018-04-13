0a5749cf2e52ff37dc5e45ab4d1277dea9b16adc
Ross Golder <ross@golder.org>
2018-04-13 17:29:58 +0700

    Discord webhooks blocked by Cloudflare with urllib2, but not with requests lib.

--- backups/notifications/discord.py
+++ backups/notifications/discord.py
@@ -1,4 +1,4 @@
-import urllib2, json
+import requests, json
 import os, os.path
 import logging
 
@@ -14,33 +14,27 @@ class Discord(BackupNotification):
 
     def notify_success(self, source, hostname, filename, stats):
         filesize = stats.getSizeDescription()
-        headers = {
-            'Content-type': 'application/json; charset=UTF-8'
-        }
         data = {
             'content': "Backup of '%s' (%s) on '%s' was successful [size: %s]." % (source.name, source.type, hostname, filesize),
         }
         try:
-            r = urllib2.Request(self.url, json.dumps(data), headers)
-            f = urllib2.urlopen(r)
-            response = f.read()
+            r = requests.post(self.url, data=data)
+            r.raise_for_status()
             logging.info("Sent success notification via Discord.")
-        except urllib2.HTTPError, error:
-            contents = error.read()
-            logging.error("Unable to send Discord success notification: " + contents)
+        except requests.exceptions.HTTPError as err:
+            logging.error("Unable to send Discord success notification: " + err)
 
     def notify_failure(self, source, hostname, e):
         headers = {
-            'Content-type': 'application/json; charset=UTF-8'
+            'Content-Type': 'application/json; charset=UTF-8',
+            'User-Agent': 'Mozilla/5.0'
         }
         data = {
             'content': "Backup of '%s' (%s) on '%s' failed: %s" % (source.name, source.type, hostname, str(e)),
         }
         try:
-            r = urllib2.Request(self.url, json.dumps(data), headers)
-            f = urllib2.urlopen(r)
-            response = f.read()
+            r = requests.post(self.url, data=data)
+            r.raise_for_status()
             logging.info("Sent failure notification via Discord.")
-        except urllib2.HTTPError, error:
-            contents = error.read()
-            logging.error("Unable to send Discord failure notification: " + contents)
+        except requests.exceptions.HTTPError as err:
+            logging.error("Unable to send Discord failure notification: " + err)
--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 from setuptools import setup
 
 setup(name = 'backups',
-    version = '2.0.2',
+    version = '2.0.3',
     description = 'Data Backup Scripts',
     author = 'Ross Golder',
     author_email = 'ross@golder.org',
     url = 'https://github.com/rossigee/backups',
     packages = ['backups', 'backups.sources', 'backups.destinations', 'backups.notifications'],
+    install_requires = [
+        'requests'
+    ],
     entry_points = {
         'console_scripts': [
             'backup = backups.main:main'
