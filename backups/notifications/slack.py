4bf10048ab8108caa47ed2d4b0d393fca90c755f
Brice Giesbrecht <brice.giesbrecht@clearobject.com>
2018-09-22 14:30:41 -0400

    Refactored slack notifications, testing RDS

--- backups/notifications/slack.py
+++ backups/notifications/slack.py
@@ -7,31 +7,52 @@ from backups.exceptions import BackupException
 from backups.notifications import backupnotification
 from backups.notifications.notification import BackupNotification
 
+
 @backupnotification('slack')
 class Slack(BackupNotification):
     def __init__(self, config):
         BackupNotification.__init__(self, config, 'slack')
         self.url = config['url']
 
-    def notify_success(self, source, hostname, filename, stats):
-        filesize = stats.getSizeDescription()
+    @staticmethod
+    def pretty_print(seconds):
+        sec = 0 if not seconds else seconds
+        m, s = divmod(sec, 60)
+        h, m = divmod(m, 60)
+        return "%d:%02d:%02d" % (h, m, s)
+
+    def _send(self, type, payload):
         headers = {
             'Content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
         }
-        data = {
-            'username': 'Backup Agent',
-            'icon_emoji': ':+1:',
-            'text': "Backup of '%s' (%s) on '%s' was successful [size: %s]." % (source.name, source.type, hostname, filesize),
-        }
-        data = urllib.urlencode({'payload': json.dumps(data)})
+        data = urllib.urlencode({'payload': json.dumps(payload)})
         req = urllib2.Request(self.url, data, headers)
         try:
             f = urllib2.urlopen(req)
             response = f.read()
-            logging.info("Sent success notification via Slack.")
+            logging.info('Sent %s notification via Slack.' % type)
         except urllib2.HTTPError, error:
             contents = error.read()
-            logging.error("Unable to send Slack success notification: " + contents)
+            logging.error('Unable to send Slack %s notification: ' % type + contents)
+
+    def notify_start(self, source, hostname):
+        data = {
+            'username': 'Backup Agent',
+            'icon_emoji': ':+1:',
+            'text': "Backup of '%s' (%s) on '%s' beginning." % (source.name, source.type, hostname),
+        }
+        self._send('start', data)
+
+    def notify_success(self, source, hostname, filename, stats):
+        filesize = stats.getSizeDescription()
+        data = {
+            'username': 'Backup Agent',
+            'icon_emoji': ':+1:',
+            'text': "Backup of '%s' (%s) on '%s' was successful [size: %s].\nTimes: backup %s, dump %s" %
+                    (source.name, source.type, hostname, filesize,
+                     Slack.pretty_print(stats.dumptime), Slack.pretty_print(stats.uploadtime)),
+        }
+        self._send('success', data)
 
     def notify_failure(self, source, hostname, e):
         data = {
@@ -39,12 +60,5 @@ class Slack(BackupNotification):
             'icon_emoji': ':imp:',
             'text': "Backup of '%s' (%s) on '%s' failed: %s" % (source.name, source.type, hostname, str(e)),
         }
-        data = urllib.urlencode({'payload': json.dumps(data)})
-        req = urllib2.Request(self.url, data)
-        try:
-            f = urllib2.urlopen(req)
-            response = f.read()
-            logging.info("Sent failure notification via Slack.")
-        except urllib2.HTTPError, error:
-            contents = error.read()
-            logging.error("Unable to send Slack failure notification: " + contents)
+        self._send('failure', data)
+
