17b8b735eec62a09a866c65361ec7168ed48bee3
Ross Golder <ross@golder.org>
2020-04-16 00:06:27 +0700

    Bump to version 2.2.0.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name = 'backups',
-    version = '2.1.2',
+    version = '2.2.0',
     description = 'Data Backup Scripts',
     author = 'Ross Golder',
     author_email = 'ross@golder.org',
